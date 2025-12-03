7d1d16692c0be0b905a6d36bb72e6dae9b2625f8
hudairyounas <hudairshorts@gmail.com>
2025-12-03 19:57:01 +0500

    js class 3

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,22 @@
-console.log("External JavaScript file loaded successfully!");
+//? var
+//? let
+//? const
+
+// let a = 20;
+// let a = 30;
+// console.log(a);
+
+// var students = 1000;
+// var students = 6000;
+
+// const b = 20;
+// const b = 40;
+// console.log(b);
+
+
+// let studentsName = undefined;
+// console.log(typeof studentsName)
+
+let a = 20;
+let b = 40;
+console.log(a + b);
