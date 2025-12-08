ced287156bbb61390a83e7cb8df54f901eae01a9
hudairyounas <hudairshorts@gmail.com>
2025-12-08 22:36:24 +0500

    js class 5

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -44,6 +44,60 @@
 //     console.log("odd")
 // }
 
-console.log("12" === 12);
+// console.log("12" === 12);
 //? 15
 //? -4
+
+// let day = "Tuesday"
+// switch (day) {
+//     case "Monday":
+//         console.log("today is Monday")
+//     case "Tuesday":
+//         console.log("today is Tuesday")
+//     case "Wednesday":
+//         console.log("today is Wednesday")
+
+//     default:
+//         console.log("no condition is match")
+//         break;
+// }
+
+//? for loop
+//? while loop
+//? do while loop
+
+// for (let a = 13; a > 10; a++) {
+//     console.log(a, "Hello")
+// }
+
+// let w = 0;
+// while (w < 10) {
+//     console.log(w, "hello");
+//     console.log(w, "hello");
+//     console.log(w, "hello");
+//     console.log(w, "hello");
+//     console.log(w, "hello");
+//     console.log(w, "hello");
+//     console.log(w, "hello");
+//     console.log(w, "hello");
+//     w++;
+// }
+
+let a = 10;
+while (a < 4) {
+    console.log("hello");
+    a++;
+}
+
+
+let n = 7;
+do {
+    console.log(n, "hello");
+    n++
+} while (n < 3);
+
+//? a = 0 a++ 
+//? a = a + 1 
+
+//? for in
+//? for of
