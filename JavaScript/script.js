c9cf8c7c6688e85c6ddf923b0702d42a770a8da6
hudairyounas <hudairshorts@gmail.com>
2025-12-23 22:11:20 +0500

    js class 16

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -481,12 +481,77 @@
 // const str = "JAVASCRIPT";
 // console.log(str.toLowerCase())
 //? trim: Removes whitespace from both ends of the string.
-const str = "    a    ";
-let result = str.trim();
-console.log(result.length)
+// const str = "    a    ";
+// let result = str.trim();
+// console.log(result.length)
 // console.log(str.length);
 // console.log(str.trim());
 // console.log(str)
 
 
 //? split: Splits the string into an array of substrings based on a specified delimiter.
+
+
+//* Math: The Math namespace object contains static properties and methods for mathematical constants and functions.
+//* Math works with the Number type. It doesn't work with BigInt.
+
+//? 1: Constants:
+//* - Math.PI: Represents the mathematical constant Pi (π).
+// const piValue = Math.PI;
+// console.log(piValue);
+
+
+
+//? 2. Basic Operations:
+
+//? Math.abs(): The Math.abs() static method returns the absolute value of a number.
+// or in simple, how far the number is from 0. It will be always positive
+
+// console.log(Math.abs(-21212));
+
+//? Math.round(x): Rounds a number to the nearest integer.
+// const roundedValue = Math.round(3.7);
+// console.log(roundedValue)
+
+
+
+//? Math.ceil(x): Returns the value of x rounded up to its nearest integer:
+// const ceilValue = Math.ceil(3.7);
+// console.log(ceilValue)
+
+//? Math.floor(x): Returns the value of x rounded down to its nearest integer.
+// const floorValue = Math.floor(3.7);
+// console.log(floorValue)
+
+//? Math.trunc(x): Returns the integer part of x:
+// const floorValue = Math.floor(-3.7);
+// const truncValue = Math.trunc(-3.7);
+// console.log(truncValue);
+// console.log(floorValue);
+
+//todo Math.trunc() were added to JavaScript 2015 - ES6.
+// const truncValue = Math.trunc(-3.7);
+// const floorValue = Math.floor(-3.1);
+
+//* 3. Exponential and Logarithmic Functions:
+
+//? Math.pow(x, y): Returns the value of x to the power of y.
+// console.log(Math.pow(2, 3));
+
+//? Math.sqrt(): Math.sqrt(x) returns the square root of x:
+// let squareRoot = Math.sqrt(25);
+// console.log(squareRoot)
+
+//? Math.log(x) returns the natural logarithm of x.
+// let logResult = Math.log(1);
+// console.log(logResult);
+// let logResult = Math.log(2);
+
+//? Math.log2(x) returns the base 2 logarithm of x.
+// let logResult = Math.log2(8);
+// console.log(logResult)
+
+//! Interview Question
+//* Generate Random number
+// let rand = parseInt(Math.random() * 50);
+// console.log(rand);
