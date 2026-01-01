8a6750a65d5ac0e31a8a1138a8cddb15f416a374
hudairyounas <hudairshorts@gmail.com>
2026-01-01 23:23:25 +0500

    class 19

--- JavaScript/projects/todo/script.js
+++ JavaScript/projects/todo/script.js
@@ -1,8 +1,29 @@
 let input = document.getElementById("addTaskField")
 let btn = document.getElementById("addTask")
+let container = document.getElementById("container")
 
 
+
+function TODO() {
+    let div = document.createElement("div");
+    let p = document.createElement("p");
+    let btn = document.createElement("button");
+    p.textContent = input.value;
+    btn.textContent = "Delete";
+
+    div.classList.add("divTask")
+    div.appendChild(p)
+    div.appendChild(btn)
+    container.append(div)
+    
+
+    btn.addEventListener("click", () => {
+        div.remove();
+    })
+    
+    input.value = "";
+}
 
 btn.addEventListener("click", () => {
-    console.log(input.value)
+   TODO()
 })
