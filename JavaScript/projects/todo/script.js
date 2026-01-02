261099628a7e3c803b2422eedb3af89e47130578
hudairyounas <hudairshorts@gmail.com>
2026-01-02 20:58:38 +0500

    class 20

--- JavaScript/projects/todo/script.js
+++ JavaScript/projects/todo/script.js
@@ -1,29 +1,40 @@
-let input = document.getElementById("addTaskField")
-let btn = document.getElementById("addTask")
-let container = document.getElementById("container")
+let input = document.getElementById("addTaskField");
+let addBtn = document.getElementById("addTask");
+let container = document.getElementById("container");
 
-
-
-function TODO() {
-    let div = document.createElement("div");
+const addTODO = () => {
     let p = document.createElement("p");
-    let btn = document.createElement("button");
-    p.textContent = input.value;
-    btn.textContent = "Delete";
+    let deleteBtn = document.createElement("button");
+    let div = document.createElement("div");
+    let input = document.createElement("input");
 
-    div.classList.add("divTask")
-    div.appendChild(p)
-    div.appendChild(btn)
-    container.append(div)
-    
-
-    btn.addEventListener("click", () => {
+    deleteBtn.addEventListener("click", () => {
         div.remove();
     })
+
+    input.addEventListener("change", () => {
+        if (input.checked) {
+            p.style.textDecoration = "line-through";
+        }else {
+            p.style.textDecoration = "none";
+        }
+    })
+
+
+    deleteBtn.innerText = "Delete";
+    input.type = "checkbox";
+    p.textContent = input.value;
     
+    div.classList.add("divTask");
+    div.appendChild(p)
+    div.appendChild(deleteBtn)
+    div.appendChild(input)
+    container.append(div)
+
     input.value = "";
 }
 
-btn.addEventListener("click", () => {
-   TODO()
+
+addBtn.addEventListener("click", () => {
+    addTODO();
 })
