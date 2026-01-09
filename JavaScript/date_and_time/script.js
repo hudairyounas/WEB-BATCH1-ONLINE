1ddc09ef3bc71ff2185e57631f8049d9f58be12e
hudairyounas <hudairshorts@gmail.com>
2026-01-09 20:49:12 +0500

    class 25

--- JavaScript/date_and_time/script.js
+++ JavaScript/date_and_time/script.js
@@ -1,22 +1,16 @@
-// let date = new Date();
-// // let year = date.getMonth();
-// // let day = date.getDay()
-// let month = date.setFullYear(2027);
-// console.log(date);
-// console.log(month)
-// let examDate = new Date("2026-03-15");
-// console.log(examDate);
+// setTimeout(() => {
+//     console.log("Hello js")
+// }, 5000);
+// let t = setTimeout(func, 5000);
+// clearTimeout(t);
+// let t = setInterval(func, 5000);
+// clearInterval(t);
+const func = () => {
+    console.log("Hello js");
+}
 
-// let date = new Date();
-// let date2 = date.toTimeString();
-// console.log(date2) 
-
-let today = new Date();
-let mili = today.getMilliseconds();
-// let feeLastDate = new Date("2026-01-20");
-console.log(mili)
-// if (today > feeLastDate) {
-//   console.log("Fee Late");
-// } else {
-//   console.log("Fee On Time");
-// }
+let t =setInterval(func, 1000);
+func();
+setTimeout(()=> {
+    clearInterval(t);
+}, 5000)
