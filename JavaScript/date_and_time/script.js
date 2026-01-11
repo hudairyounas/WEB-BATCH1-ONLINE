80259193efbedd472f549130861b53d4a28f6e3d
hudairyounas <hudairshorts@gmail.com>
2026-01-11 19:24:51 +0500

    class 26

--- JavaScript/date_and_time/script.js
+++ JavaScript/date_and_time/script.js
@@ -5,12 +5,17 @@
 // clearTimeout(t);
 // let t = setInterval(func, 5000);
 // clearInterval(t);
-const func = () => {
-    console.log("Hello js");
-}
+// const func = () => {
+//     console.log("Hello js");
+// }
 
-let t =setInterval(func, 1000);
-func();
-setTimeout(()=> {
-    clearInterval(t);
-}, 5000)
+// let t =setInterval(func, 1000);
+// func();
+// setTimeout(()=> {
+//     clearInterval(t);
+// }, 5000)
+
+let date = new Date(0);
+// let year = date.setFullYear(2007)
+// let fullYear = new Date(year);
+console.log(date)
