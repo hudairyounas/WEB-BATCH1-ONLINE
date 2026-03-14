4df81311e583cf661d94a4ccd4c9d2fbc4b13d67
hudairyounas <hudairshorts@gmail.com>
2026-03-14 20:10:42 +0500

    react class 4

--- react-js/src/App.js
+++ react-js/src/App.js
@@ -1,29 +1,39 @@
-//? App component
-//? Jsx - JavaScript XML
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css"
 // import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+import Courses from "./pages/courses/Courses";
+import About from "./pages/about/About";
 
 //? index.css - global css file 
 
 function App() {
-  // let name = "John Doe";
+
+  // let url = window.location.href;
+  // url = url.split("/").pop();
+  // url = "/" + url
+  // console.log(url)
+
   return (
     <>
-      <Header />
-      {/* <h1 style={{
-        backgroundColor: "red",
-        color: "yellow"
-      }}>Name {name}</h1>
-      <h2 className="nav">Welcome to React!</h2> */}
-{/* 
-        <Footer />
-        <Footer />
-        <Footer />
-        <Footer />
-        <Footer /> */}
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/course" element={<Courses />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+      {/* <Header>
+    <h1>Hello from child</h1>
+  </Header> */}
+
+      {/* {url == "/" && <h1>Home page</h1>}
+      {url == "/about" && <h1>About page</h1>}
+      {url == "/contact" && <h1>Contact page</h1>} */}
     </>
-  )
+  );
 }
 
 export default App;
